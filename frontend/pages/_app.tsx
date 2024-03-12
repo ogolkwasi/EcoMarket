b0a1d7a4cd23e7f070be56c0a55e83aeefc3a470
Vincent Kwasi <95524612+ogolkwasi@users.noreply.github.com>
2024-03-12 18:39:38 +0000

    feat: Added new version of '@wagmi/chains' library and fixed code styling in multiple files

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -3,68 +3,73 @@ import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { configureChains, createConfig, WagmiConfig } from "wagmi";
 import celoGroups from "@celo/rainbowkit-celo/lists";
 import Layout from "../components/Layout";
-import "./globals.css"
+import "./globals.css";
 import "@rainbow-me/rainbowkit/styles.css";
 import { publicProvider } from "wagmi/providers/public";
 import { Alfajores, Celo } from "@celo/rainbowkit-celo/chains";
-import { mainnet, polygon, optimism, arbitrum, celoAlfajores, celo ,optimismGoerli, polygonMumbai, arbitrumGoerli, goerli } from "wagmi/chains";
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { BrowserRouter as Routes, Route } from 'react-router-dom';
-import NFTPage from './NFTPage';
-
+import {
+  mainnet,
+  polygon,
+  optimism,
+  arbitrum,
+  celoAlfajores,
+  celo,
+  optimismGoerli,
+  polygonMumbai,
+  arbitrumGoerli,
+  goerli,
+} from "wagmi/chains";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { BrowserRouter as Routes, Route } from "react-router-dom";
+import NFTPage from "./NFTPage";
 
 const queryClient = new QueryClient();
-const projectId = process.env.NEXT_PUBLIC_WC_PROJECT_ID as string; 
-// get one at https://cloud.walletconnect.com/app
+const projectId = "071c50c0ebe4fb679de0a950913210fb"; // Replace with your provided projectId
 
 const { chains, publicClient } = configureChains(
-    [Celo, Alfajores, mainnet, polygon, optimism, arbitrum, optimismGoerli, polygonMumbai, arbitrumGoerli, goerli],
-    [publicProvider()]
+  [
+    Celo,
+    Alfajores,
+    mainnet,
+    polygon,
+    optimism,
+    arbitrum,
+    optimismGoerli,
+    polygonMumbai,
+    arbitrumGoerli,
+    goerli,
+  ],
+  [publicProvider()]
 );
 
 const connectors = celoGroups({
-    chains,
-    projectId,
-    appName:
-        (typeof document === "object" && document.title) || "EcoMarket",
+  chains,
+  projectId,
+  appName: (typeof document === "object" && document.title) || "EcoMarket",
 });
 
 const appInfo = {
-    appName: "EcoMarket",
+  appName: "EcoMarket",
 };
 
 const wagmiConfig = createConfig({
-    autoConnect: true,
-    connectors,
-    publicClient: publicClient,
+  autoConnect: true,
+  connectors,
+  publicClient: publicClient,
 });
 
 function App({ Component, pageProps }: AppProps) {
-    return (
-        
-        <WagmiConfig config={wagmiConfig}>
-            <RainbowKitProvider
-                chains={chains}
-                appInfo={appInfo}
-                coolMode={true}
-            >
-          
-               <QueryClientProvider client={queryClient}>
-               
-                <Layout>
-                
-              
-               
-                    <Component {...pageProps} />
-                   
-                </Layout>
-                
-                </QueryClientProvider>
-            
-            </RainbowKitProvider>
-        </WagmiConfig>
-       
-    );
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains} appInfo={appInfo} coolMode={true}>
+        <QueryClientProvider client={queryClient}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </QueryClientProvider>
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
 
 export default App;
