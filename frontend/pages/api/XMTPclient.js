28831159906913a6e9fcf3a2757dd4d6ec834b14
ogolkwasi <mrogolkwasi@protonmail.com>
2024-03-28 11:20:26 +0300

    Compile settings

--- frontend/pages/api/XMTPclient.js
+++ frontend/pages/api/XMTPclient.js
@@ -1,31 +1,27 @@
-import InputForm from './InputForm'
-
+import { useState } from 'react'; // Import useState hook from react
 
 const ethers = require("ethers");
 const { Client } = require("@xmtp/xmtp-js");
 
-// Define the recipient's address
-const recipientAddress = "0x3bcF60b44ba99e7994b075086c353062c6AE0480"; //  recipient's address
+const recipientAddress = "0x3bcF60b44ba99e7994b075086c353062c6AE0480"; // recipient's address
 
 export default async function sendMSG() {
+  const [record, setRecord] = useState(''); // Define state for record variable
 
-    const [record, setRecord] = useState('');
-  // Connect to the user's wallet using their Ethereum provider (e.g., MetaMask)
-  if (window.ethereum) {
+  if (typeof window !== 'undefined' && window.ethereum) { // Check if window and window.ethereum are defined
     try {
       await window.ethereum.request({ method: 'eth_requestAccounts' });
       const provider = new ethers.providers.Web3Provider(window.ethereum);
       const signer = provider.getSigner();
       
-      // Initialize the xmtp client
       const xmtp = await Client.create(signer, { env: "dev" });
       
       console.log("Message from EcoMarket platform: ", await signer.getAddress());
 
-      // Check if the recipient can receive messages (not necessary if you know the recipient's wallet can receive messages)
-       const canMessage = await xmtp.canMessage(recipientAddress, signature);
+      // Check if the recipient can receive messages (you need to define signature)
+      const canMessage = await xmtp.canMessage(recipientAddress, signature);
 
-      // Send a message
+      // Send a message (you need to define record)
       const conversation = await xmtp.conversations.newConversation(recipientAddress);
       const sent = await conversation.send(record);
       
